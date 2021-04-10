2259e9ccfce82e1ed50dd7a3df65e9ebd2b3ec26
macandrew6 <mr.betterandrewmac@gmail.com>
2021-04-09 21:35:54 -0700

    init get route

--- server/src/index.js
+++ server/src/index.js
@@ -12,6 +12,12 @@ app.use(
   })
 );
 
+app.get('/'. (req, res) => {
+  res.json({
+    message: 'hello world'
+  })
+})
+
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
