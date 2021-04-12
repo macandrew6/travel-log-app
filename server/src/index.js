17e024d3ddfe713474ba950a39eaa2a4f795280c
macandrew6 <mr.betterandrewmac@gmail.com>
2021-04-12 15:59:09 -0700

    adding not found middleware

--- server/src/index.js
+++ server/src/index.js
@@ -12,11 +12,26 @@ app.use(
   })
 );
 
-app.get('/'. (req, res) => {
+app.get('/', (req, res) => {
   res.json({
-    message: 'hello world'
-  })
-})
+    message: 'hello world',
+  });
+});
+
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: error.stack,
+  });
+});
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
