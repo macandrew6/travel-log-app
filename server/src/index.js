a4e5a70a3f7fb3cb5bb2cfa26f8b67bcd2eff87b
macandrew6 <mr.betterandrewmac@gmail.com>
2021-04-14 19:27:55 -0700

    connects to DB using mongoose and adds .env

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const middlewares = require('./middlewares');
 
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+});
+
 app.use(morgan('common'));
 app.use(helmet());
 app.use(
